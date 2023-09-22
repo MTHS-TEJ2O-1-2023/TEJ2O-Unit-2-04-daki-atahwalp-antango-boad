4dedd527d0ad9dc30f46614379892cceaeaf92ca
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-09-22 17:15:34 +0000

    not working for some reason

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,22 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Daki A.B
+Created on: Sep 2023
+This module is a Microbit finds temperature
 """
 
+
+
 from microbit import *
+import random
 
+# our varibale for temperature
+temperature = 0
 
-display.scroll("Hello, World!")
+display.clear()
+display.show (Image.HAPPY)
+
+while True: 
+    if button_a.is_pressed():
+        temperature = temperature()
+        display.show(str(temperature))
+
