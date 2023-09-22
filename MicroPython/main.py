b479e84bc74a9b33ae3c047a4ed286d5cc0f7172
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-09-22 17:18:10 +0000

    Microbit finds temperature.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -3,20 +3,16 @@ Created by: Daki A.B
 Created on: Sep 2023
 This module is a Microbit finds temperature
 """
-
-
-
 from microbit import *
 import random
 
-# our varibale for temperature
+# varibale for temperature
 temperature = 0
 
 display.clear()
-display.show (Image.HAPPY)
+display.show(Image.HAPPY)
 
-while True: 
+while True:
     if button_a.is_pressed():
         temperature = temperature()
         display.show(str(temperature))
-
